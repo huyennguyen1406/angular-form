0c1fa8e9d4152fea77630cbb502b2961f40dd166
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2022-03-28 15:58:29 +0700

    add time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { FormRegisterComponent } from './form-register/form-register.component';
+import { TimelinesComponent } from './timelines/timelines.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FormRegisterComponent
+    FormRegisterComponent,
+    TimelinesComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
