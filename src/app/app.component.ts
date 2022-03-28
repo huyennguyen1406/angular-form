62e229b21d49d0edcc7178c953c9e35f7e15b3cd
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2022-03-28 10:53:23 +0700

    add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-form';
+  title = 'reactive-form';
+  countryList = [
+    {id: 1, name: 'Viet Nam'},
+    {id: 2, name: 'Canada'},
+    {id: 3, name: 'USA'},
+  ];
+  contactForm = new FormGroup({
+    firstname: new FormControl('', [Validators.required]),
+    lastname: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    gender: new FormControl('', [Validators.required]),
+    isMarried: new FormControl('', [Validators.required]),
+    country: new FormControl('', [Validators.required]),
+    address: new FormGroup({
+      city: new FormControl('', [Validators.required]),
+      street: new FormControl('',[Validators.required]),
+      pincode: new FormControl('', [Validators.required])
+    })
+  });
+  get firstname(){
+    return this.contactForm.get('firstname');
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
 }
